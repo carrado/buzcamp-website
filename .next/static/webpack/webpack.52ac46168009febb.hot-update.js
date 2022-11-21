9d1a835c868363ddedd031a2c8aa05c07fa72f9d
princeemeka965 <anyanwue4@gmail.com>
2022-11-20 20:04:21 -0800

    set up dependencies

--- .next/static/webpack/webpack.52ac46168009febb.hot-update.js
+++ .next/static/webpack/webpack.52ac46168009febb.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "944e87805c1adbb3"; }
+/******/ 	__webpack_require__.h = function() { return "d9c34f79ccc02a04"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,12 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>{'BuzCamp'}</title>
+                <meta name="description" content="Socio-academic Website Buzcamp buzcamp social media academic universities schools" />
+                <meta name="og:title" content="Buzcamp" />
+                <meta property="og:description" content="A Socio-academic platform connecting students across various institutions" />
+                <meta property="og:image" content="https://buzcamp.com/" />
+                <title>BuzCamp</title>
+                <link rel="icon" href="/bzcmp.png" />
             </Head>
             <section className={utilStyles.headingMd}>
                 <p>
