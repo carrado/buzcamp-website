6bad97215ee9b8d8eec65e7154b7b31dd6360738
princeemeka965 <anyanwue4@gmail.com>
2022-12-31 00:38:24 +0100

    config next js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/_next/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "https://buzcamp.com" },
-        ],
-      }
-    ]
-  }
 };
 
 module.exports = nextConfig;
