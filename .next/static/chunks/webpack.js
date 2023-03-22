fbf5d255d58cb3d268920897e48e902b520d97c3
princeemeka965 <anyanwue4@gmail.com>
2023-03-22 13:59:30 +0100

    404 error design

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cfd7b494b214d704"; }
+/******/ 		__webpack_require__.h = function() { return "23658c9edc63862e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
